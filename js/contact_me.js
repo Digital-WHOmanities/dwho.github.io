1bb2497590a1ca14d478b35e68b78c15cb055852
MilenaCorbellini <45001201+MilenaCorbellini@users.noreply.github.com>
2020-04-16 12:16:01 +0200

    Update contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -16,7 +16,7 @@ $(function() {
         firstName = name.split(' ').slice(0, -1).join(' ');
       }
       $this = $("#sendMessageButton");
-      $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
+      $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate message
       $.ajax({
         url: "././mail/contact_me.php",
         type: "POST",
