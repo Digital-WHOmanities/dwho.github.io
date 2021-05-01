06c5090b5239be27d5e2442ae6968858591b35f9
Carlo Teo Pedretti <alphacentauri90@gmail.com>
2021-05-01 18:16:32 +0200

    fix fetch function

--- js/helper.js
+++ js/helper.js
@@ -7,12 +7,12 @@ function fetchItems() {
       for (let element in data) {
         if (element === 'posts') {
           for (let post of data[element]) {
-            var div_1 = document.createElement("div");
-            div_1.classList.add("mystyle");   
+            // var div_1 = document.createElement("div");
+            // div_1.classList.add("mystyle");   
             // btn.innerHTML = "CLICK ME";                   
-            document.body.appendChild(div_1);               
-            // console.log(post.title + '' + post.content);
-              // <section class="page-section live" id="about">
+            // document.body.appendChild(div_1);               
+            console.log(post.title + '' + post.content);
+            // <section class="page-section live" id="about">
           }
         }
       }
