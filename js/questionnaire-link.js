e3fbff81a2c161514c3efd4e9e162bd3e9e94db2
EleonoraPeruch <45010379+EleonoraPeruch@users.noreply.github.com>
2021-05-06 15:40:28 +0200

    Update questionnaire-link.js

--- js/questionnaire-link.js
+++ js/questionnaire-link.js
@@ -1,3 +1,3 @@
 function goToQuestionnaire() {
-    window.open("https://docs.google.com/forms/d/e/1FAIpQLSdIVFXHXhxT4TNRyOZzlqnJ2D_rcDK_AGf20BBdWW8HzT7Y3A/viewform?usp=sf_link", "_blank");
+    window.open("https://docs.google.com/forms/d/e/1FAIpQLSdBBKSqLuLHYuYhdQBM6lG3OpSBC9VC2DmAtstPPu3xyxvmyQ/closedform", "_blank");
 }
